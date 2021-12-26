443d6e7a384ace591cadc254f2aa51a9480f8900
Abdelrahman Reda <57096941+AbdelrhmanReda@users.noreply.github.com>
2021-12-26 13:14:05 +0200

    Update Calculator.asm
    I just added my task : multiplication , please announce me if there is and edition

--- Calculator.asm
+++ Calculator.asm
@@ -141,7 +141,29 @@ Subtraction:mov ah,09h
 
 
 
-Multiplication:             
+Multiplication: mov ah,09h
+                mov dx, offset firstNumberMsg             
+                int 21h 
+                mov cx,0
+                call InputNumber
+                push dx     
+                mov ah,9
+                mov dx, offset secondNumberMsg
+                int 21h
+                mov cx,0
+                call InputNumber
+                pop bx
+                mov ax,dx
+                mul bx 
+                mov dx,ax
+                push dx 
+                mov ah,9
+                mov dx, offset resultMsg
+                int 21h
+                mov cx,10000
+                pop dx
+                call View 
+                jmp exit              
 
 
 
